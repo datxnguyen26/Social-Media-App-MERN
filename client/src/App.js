f9fb9734bd7a76b1106b70f04f836c0c57f68f29
Dat Nguyen <dxn003@ucsd.edu>
2023-05-06 06:53:21 -0700

    Complete buttons functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
 import memories from './assets/memories.png'
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
 import useStyles from './styles';
-
+import { useDispatch } from 'react-redux'
+import { getPosts } from './actions/posts'
 const App = () => {
+    const [currentId, setCurrentId] = useState(0)
     const classes = useStyles();
+    const dispatch = useDispatch();
 
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [currentId, dispatch])
     return (
         <Container maxWidth="lg">
             <AppBar className={classes.appBar}  position="static" color="inherit">
@@ -18,10 +24,10 @@ const App = () => {
                 <Container>
                     <Grid container justify="space-betwwen" alignItems="stretch" spacing={3}>
                         <Grid item xs={12} sm={7}>
-                            <Posts />
+                            <Posts setCurrentId={setCurrentId} />
                         </Grid>
                         <Grid item xs={12} sm={4}>
-                            <Form />
+                            <Form currentId={currentId} setCurrentId={setCurrentId} />
                         </Grid>
                     </Grid>
                 </Container>
--- client/src/components/Form/styles.js
+++ client/src/components/Form/styles.js
@@ -4,21 +4,21 @@ export default makeStyles((theme) => ({
   root: {
     '& .MuiTextField-root': {
       margin: theme.spacing(1),
-    },
+    }
   },
   paper: {
-    padding: theme.spacing(2),
+    padding: theme.spacing(2)
   },
   form: {
     display: 'flex',
     flexWrap: 'wrap',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   fileInput: {
     width: '97%',
-    margin: '10px 0',
+    margin: '10px 0'
   },
   buttonSubmit: {
-    marginBottom: 10,
+    marginBottom: 10
   },
 }));
