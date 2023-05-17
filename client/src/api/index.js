dcc7387555f965f4248dd1717d83ccf33d95fe20
Dat Nguyen <dxn003@ucsd.edu>
2023-05-17 00:36:17 -0700

    Deployment finalize

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/posts'
+const API = axios.create({ baseURL: 'http://localhost:5000' });
 
-export const fetchPosts = () => axios.get(url);
-export const createPost = (newPost) => axios.post(url, newPost)
-export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost)
-export const deletePost = (id) => axios.delete(`${url}/${id}`)
-export const likePost = (id) => axios.patch(`${url}/${id}/likePost`)
+
+export const fetchPost = (id) => API.get(`/posts/${id}`);
+export const fetchPosts = (page) => API.get(`/posts?page=${page}`);
+export const createPost = (newPost) => API.post('/posts', newPost);
+export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
+export const deletePost = (id) => API.delete(`/posts/${id}`);
+export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
+
--- server/index.js
+++ server/index.js
@@ -13,8 +13,11 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use('/posts', postRoutes)
+app.get('/', (req, res) => {
+    res.send('APP STARTED!')
+})
 
-const CONNECTION_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.lttfuey.mongodb.net/?retryWrites=true&w=majority`
+const CONNECTION_URL = process.env.MONGODB_URI
 const PORT = process.env.PORT || 5000
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
